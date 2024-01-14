ed08e137dfd313b2400ed5ac22ffe7acaaf30339
Krish90s <krishnanvenkat4@gmail.com>
2024-01-14 15:00:00 +0530

    env configured

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-
-const MONGODB_URI =
-  "mongodb+srv://90s_krishnan:n9FuPVnfiPD0Dw3W@cluster0.a24ea.mongodb.net/authorization";
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     const conSuccess = mongoose.connection;
     conSuccess.once("open", (_) => {
       console.log("Database connected:", db);
