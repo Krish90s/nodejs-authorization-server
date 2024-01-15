5c137570cb69f1b046478ecfef8f6c55ed5e055e
Krish90s <krishnanvenkat4@gmail.com>
2024-01-15 16:29:27 +0530

    node authorization server

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const authRouter = require("./routes/auth");
 const cors = require("cors");
 const passport = require("passport");
 const session = require("express-session");
+const logger = require("morgan");
+const MongoDBStore = require("connect-mongodb-session")(session);
 require("./config/passport");
-
 require("dotenv").config();
+
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: "sessions",
+});
+
+// Catch errors
+store.on("error", function (error) {
+  console.log(error);
+});
 
 const app = express();
 const port = 3000;
@@ -15,27 +26,28 @@ const port = 3000;
 connectDB();
 
 app.use(cors());
-
+app.use(logger("dev"));
 app.use(
-	session({
-		secret: "mysecret",
-		resave: false,
-		saveUninitialized: true,
-		cookie: { secure: false },
-	})
+  session({
+    secret: "session",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+  })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect("http://localhost:5173/login");
+  req.logout();
+  res.redirect("http://localhost:5173/login");
 });
 
 app.use("/api/v1/auth", authRouter);
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const { IsAuthenticated } = require("../middlewares/IsAuthenticated");
 require("dotenv").config();
 
 router.get(
-	"/google",
-	passport.authenticate("google", { scope: ["profile", "email"] })
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `http://localhost:5173`,
+    failureRedirect: "/api/v1/auth/google/failure",
+  })
 );
 
 router.get("/google/success", (req, res) => {
-	return res.status(200).send(req.user._doc);
+  console.log(req);
+  if (req?.user) {
+    return res.status(200).json(req?.user?._doc);
+  } else {
+    return res.status(401).send("Un Authorized");
+  }
 });
 
 router.get("/google/failure", (req, res) => {
-	return res.status(500).send("something went wrong");
+  return res.status(500).send("something went wrong");
 });
 
 router.get("/logout", (req, res) => {
-	req.session.destroy();
-	req.logout();
-	res.redirect("http://localhost:5173/login");
+  req.session.destroy();
+  res.redirect(`${process.env.CLIENT_URL}/login`);
 });
 
-// router.get(
-// 	"/google/callback",
-// 	passport.authenticate("google", {
-// 		successRedirect: "http://localhost:5173",
-// 		failureRedirect: "/api/v1/auth/google/failure",
-// 	})
-// );
-
-router.get(
-	"/google/callback",
-	passport.authenticate("google", {
-		failureRedirect: "http://localhost:5173/login",
-		session: false,
-	}),
-	(req, res) => {
-		const user = req.user._doc;
-		res.cookie("access_token", user, { httpOnly: true, sameSite: true });
-		res.redirect("http://localhost:5173/login");
-	}
-);
-
-router.get("/currentUser", IsAuthenticated, (req, res) => {
-	return res.status(200).send(req.user._doc);
+router.get("/currentUser", (req, res) => {
+  console.log(req?.user?._doc);
+  return res.status(200).send(req.user);
 });
 
 module.exports = router;
